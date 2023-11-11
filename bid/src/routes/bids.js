e731c521436c9417882002665ca0461776cbe9f4
victoriaemc <victoriaemc10@gmail.com>
2023-11-11 19:03:09 +0100

    feat: CRUD bids

--- bid/src/routes/bids.js
+++ bid/src/routes/bids.js
@@ -28,8 +28,8 @@ const Bid = require('../models/Bid');
  *              - user
  *              - price
  *          example:
- *              product: 5f9d88b9c4b9e3b3f4f3f4f3
- *              user: 5f9d88b9c4b9e3b3f4f3r4f3
+ *              product: 6549293875aa4e12761f4ac4
+ *              user: 654926ac75aa4e12761f4ab4
  *              price: 1000
  *
  */
@@ -52,6 +52,148 @@ router.get("/", (req, res) => {
         .catch(err => res.json({ message: err.message }));
 });
 
+// Devuelve una puja por su id
+/**
+ * @swagger
+ * /bids/{id}:
+ *  get:
+ *      summary: Get bid by id
+ *      description: Get bid that matches the id
+ *      tags: [Bid]
+ *      parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *      responses:
+ *          200:
+ *              description: Success
+ *              content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/Bid'
+ *
+ */
+
+router.get("/:id", (req, res) => {
+    let id = req.params.id;
+    Bid.findById(id)
+        .then(bid => {
+            if (bid) {
+                res.json(bid);
+            } else {
+                res.status(404).json({ message: "Bid not found" });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message });
+        });
+});
+
+// Añade una puja
+/**
+ * @swagger
+ * /bids/add:
+ *  post:
+ *      summary: Add bid
+ *      description: Add a new bid
+ *      tags: [Bid]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  $ref: '#/components/schemas/Bid'
+ *      responses:
+ *          200:
+ *              description: Success
+ *              content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/Bid'
+ *
+ */
+router.post("/add", (req, res) => {
+    const bid = new Bid({
+        product: req.body.product,
+        user: req.body.user,
+        price: req.body.price
+    });
+    bid.save()
+        .then(data => res.json(data))
+        .catch(err => res.json({ message: err.message }));
+});
+
+// Borra una puja
+/**
+ * @swagger
+ * /bids/{id}:
+ *  delete:
+ *      summary: Delete bid
+ *      description: Delete a bid
+ *      tags: [Bid]
+ *      parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *      responses:
+ *          200:
+ *              description: Success
+ *
+ */
+router.delete("/:id", (req, res) => {
+    let id = req.params.id;
+    Bid.findByIdAndDelete(id)
+        .then(() => res.json({ message: "Bid deleted" }))
+        .catch(err => res.json({ message: err.message }));
+});
+
+// Modifica una puja
+/**
+ * @swagger
+ * /bids/{id}:
+ *  put:
+ *      summary: Update bid
+ *      description: Update a bid
+ *      tags: [Bid]
+ *      parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *      requestBody:
+ *          required: true
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  $ref: '#/components/schemas/Bid'
+ *      responses:
+ *          200:
+ *              description: Success
+ *
+ */
+
+router.put("/:id", async(req, res) => {
+    try {
+        const id = req.params.id;
+        const update = req.body; // Assuming the request body contains the updated bid data
+
+        // `new: true` option returns the modified document rather than the original
+        const updatedBid = await Bid.findOneAndUpdate({ _id: id }, update, { new: true });
+
+        if (updatedBid) {
+            res.json(updatedBid);
+        } else {
+            res.status(404).json({ message: "Bid not found" });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 // Devuelve las pujas realizadas en las últimas :hours horas
 /**
@@ -87,5 +229,46 @@ router.get("/before/:hours", (req, res) => {
         .then(bids => res.json(bids))
         .catch(err => res.json({ message: err.message }));
 });
+
+
+
+// Devuelve las pujas realizadas por un usuario
+/**
+ * @swagger
+ * /bids/user/{id}:
+ *  get:
+ *      summary: Get bids by user
+ *      description: Get bids that were placed by a user
+ *      tags: [Bid]
+ *      parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *      responses:
+ *          200:
+ *              description: Success
+ *              content:
+ *              application/json:
+ *                  schema:
+ *                      type: array
+ *                      items:
+ *                          $ref: '#/components/schemas/Bid'
+ *
+ */
+
+router.get("/user/:id", (req, res) => {
+    let id = req.params.id;
+    Bid.find({ user: id }, (err, bids) => {
+        if (err) {
+            res.json({ message: err.message });
+        } else {
+            res.json(bids);
+        }
+    });
+});
+
+
 
 module.exports = router;
