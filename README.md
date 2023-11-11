23437ad19e0a92f108d35d377bb508ae3d7eed73
victoriaemc <victoriaemc10@gmail.com>
2023-11-11 11:15:51 +0100

    feat: install mongoose

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # EL RASTRO
+**Autores y Autoras:**
+* Pedro José Ankersmit Carrión
+* María Fernández Moreno
+* Victoria Eugenia Moreno Contreras
+* Andrés Pérez García
+* Jaime Luis Mesa Viquez
+
+## Uso 
+Para ejecutar la aplicacion se debe ejecutar el comando `npm start` **en cada una de las carpetas del proyecto** (gateway, user, product, bid). 
+
+Para acceder a la aplicación entrar en `localhost:8000` en el navegador.
--- gateway/index.js
+++ gateway/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const proxy = require('express-http-proxy');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -8,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/bids', proxy('http://localhost:8001'));
-app.use('/products', proxy('http://localhost:8002'));
+app.use('/', proxy('http://localhost:8002')); // Product service
 app.use('/users', proxy('http://localhost:8003'));
 
 app.listen(8000, () => {
