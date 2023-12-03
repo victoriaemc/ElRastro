ac014fae2bee8c48a91e1694aeefca2bfba8381d
victoriaemc <victoriaemc10@gmail.com>
2023-12-03 20:39:45 +0100

    feat: navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import axios from "axios";
 
 function App() {
   return (
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,29 @@
 import React,{useState} from 'react';
-import { MDBInputGroup, MDBInput, MDBIcon, MDBAlert, MDBBtn } from 'mdb-react-ui-kit';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-export const Header = () => {
 
-    const loginProfile = () => {
-
-    };
-
-    return (<MDBInputGroup>
-        <MDBInput label='Search' />
-        <MDBBtn rippleColor='dark'>
-            <MDBIcon icon='search' />
-        </MDBBtn>
-    </MDBInputGroup>);
+export default function Header(){
+    return (<Navbar expand="sm" className="bg-body-tertiary justify-content-between">
+        <Navbar.Brand href="/">El Rastro</Navbar.Brand>
+        <Form inline>
+            <Row>
+                <Col xs="auto">
+                    <Form.Control
+                        type="text"
+                        placeholder="Search"
+                        className=" mr-sm-2"
+                    />
+                </Col>
+                <Col xs="auto">
+                    <Button type="submit">Submit</Button>
+                </Col>
+            </Row>
+        </Form>
+        <Navbar.Brand href="/">Login y eso</Navbar.Brand>
+    </Navbar>);
 };
-
-export default Header;
