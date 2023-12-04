468a4c0292dc30e79c493aa4a57a2a7127e5e412
Andrespg14 <anpergar14@gmail.com>
2023-12-04 12:19:07 +0100

    Widget Cloudinary

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,39 @@ import './App.css';
 import { useEffect } from 'react';
 import axios from "axios";
 
+// Imports cloudinary
+import { useState } from "react";
+import CloudinaryUploadWidget from "./components/CloudinaryUploadWidget";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage, responsive, placeholder } from "@cloudinary/react";
+
 function App() {
+  // Cloudinary vars----------------------------------------------
+  const [publicId, setPublicId] = useState("");
+  const [cloudName] = useState("hzxyensd5");
+  const [uploadPreset] = useState("aoh4fpwm");
+  const [uwConfig] = useState({
+    cloudName,
+    uploadPreset
+    // cropping: true, //add a cropping step
+    // showAdvancedOptions: true,  //add advanced options (public_id and tag)
+    // sources: [ "local", "url"], // restrict the upload sources to URL and local files
+    // multiple: false,  //restrict upload to a single file
+    // folder: "user_images", //upload files to the specified folder
+    // tags: ["users", "profile"], //add the given tags to the uploaded files
+    // context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
+    // clientAllowedFormats: ["images"], //restrict uploading to image files only
+    // maxImageFileSize: 2000000,  //restrict file size to less than 2MB
+    // maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
+    // theme: "purple", //change to a purple theme
+  });
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName
+    }
+  });
+  const myImage = cld.image(publicId);
+  // -------------------------------------------------------------
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +52,19 @@ function App() {
           Learn React
         </a>
       </header>
+      {/* Div para el widget */}
+      <div>
+        <h3>Cloudinary Upload Widget Example</h3>
+        <CloudinaryUploadWidget uwConfig={uwConfig} setPublicId={setPublicId} />
+      </div>
+      {/* Div para mostrar la imagen */}
+      <div style={{ width: "800px" }}>
+        <AdvancedImage
+          style={{ maxWidth: "100%" }}
+          cldImg={myImage}
+          plugins={[responsive(), placeholder()]}
+        />
+      </div>
     </div>
   );
 }
