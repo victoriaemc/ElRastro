f7f6dc56e6174ce2f89403c2422f1c4ca7d022ab
xmariafdz <mariafdezm02@gmail.com>
2023-12-10 21:07:05 +0100

    sockets adios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import axios from "axios";
-import io from "socket.io-client";
 
 // Imports cloudinary
 import CloudinaryUploadWidget from "./components/CloudinaryUploadWidget";
@@ -24,24 +23,7 @@ import ChatPage from "./pages/ChatPage";
 import SearchPage from "./pages/SearchPage";
 
 function App() {
-  const [socket, setSocket] = useState(null);
-  useEffect(() => {
-    // Establecer conexión con el servidor Socket.io
-    const newSocket = io("http://localhost:8000");
 
-    // Manejar eventos o realizar otras acciones según sea necesario
-    newSocket.on("connect", () => {
-      console.log("Conectado al servidor Socket.io");
-    });
-
-    // Guardar el objeto de socket en el estado
-    setSocket(newSocket);
-
-    // Limpiar la conexión al desmontar el componente
-    return () => {
-      newSocket.disconnect();
-    };
-  }, []);
   // Cloudinary vars----------------------------------------------
   const [publicId, setPublicId] = useState("");
   const [cloudName] = useState("daef41lib");
