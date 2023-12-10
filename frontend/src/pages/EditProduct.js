a838c8932c0a9998d1b45bbb9e2029be79d37f41
victoriaemc <victoriaemc10@gmail.com>
2023-12-10 18:36:37 +0100

    feat: edit product (con imagen)
    No va el css

--- frontend/src/pages/EditProduct.js
+++ frontend/src/pages/EditProduct.js
@@ -3,10 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import bootstrap and react-bootstrap components
 import 'bootstrap/dist/css/bootstrap.min.css';
 import EditProductForm from "../components/EditProductForm";
+import EditProductFormulary from "../components/EditProductFormulary";
 
 export default function EditProduct(){
     return(<div>
         <h2 className="text-center mb-5">Editar producto</h2>
-        <EditProductForm/>
+        <EditProductFormulary/>
     </div>)
 }
