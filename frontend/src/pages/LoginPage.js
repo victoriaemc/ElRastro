2c3adbae4bfd7d03d3a3ffad91d5dec002faf70b
victoriaemc <victoriaemc10@gmail.com>
2023-12-30 19:30:28 +0100

    ??: intento oauth

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,55 +1,34 @@
-// EN LA CARPETA PAGES IRAN LAS PAGINAS DE NUESTRA APP
 import React from "react";
 import {GoogleLogin, GoogleOAuthProvider} from "@react-oauth/google";
 import axios from "axios";
 import {jwtDecode} from "jwt-decode";
 function LoginPage(){
-/*  const login = useGoogleLogin({
-    onSuccess: async (response) => {
-      try {
-        const res = await axios.get(
-            "https://www.googleapis.com/oauth2/v3/userinfo",
-            {
-              headers: {
-                Authorization: `Bearer ${response.acces_token}`,
-              },
-            }
-        );
-        console.log(res);
-      } catch (err){
-        console.log(err);
-      }
-    },
-  });*/
     const handleLogin = async (credentialResponse) => {
-        var obj = jwtDecode(credentialResponse.credential);
-        //var data = JSON.stringify(obj);
-        //console.log(data);
+        try {
+            const decodedToken = jwtDecode(credentialResponse.credential);
+            console.log("Decoded Token:", decodedToken);
 
-        //const data = {};
+            // Make a request to your backend server with the token
+            const response = await axios.post("http://localhost:8000/login", {
+                token: credentialResponse.credential,
+            });
 
-        const config = {
-            method: 'POST',
-            url: 'http://localhost:8000',
-            headers: {},
-            //data: data
+            console.log("Server Response:", response.data);
+        } catch (error) {
+            console.error("Error:", error);
         }
+    };
 
-        await axios(config)
-    }
-  return(
-  <GoogleOAuthProvider clientId={"133771592681-sf648miprtjn4spn88k9flo64g3ajr22.apps.googleusercontent.com"}>
-  <GoogleLogin
-    onSuccess={handleLogin}
-      /*onSucces={(credentialResponse) => {
-      var credentialResponseDecoded = jwt_decode(credentialResponse.credential);
-      console.log(credentialResponse);
-    }}*/
-    onError={()=> {
-      console.log("Login failed");
-    }}
-    /> </GoogleOAuthProvider>
-  );
+    return (
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID_VICKY}>
+            <GoogleLogin
+                onSuccess={handleLogin}
+                onError={() => {
+                    console.log("Login failed");
+                }}
+            />
+        </GoogleOAuthProvider>
+    );
 }
 
 export default LoginPage
